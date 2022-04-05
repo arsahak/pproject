9a65935af513567371d1d07bc66d9258b741d4e9
arsahak <arsahak99@gmail.com>
2022-04-05 23:34:57 +0600

    First chnage

--- my_project/my_app/views.py
+++ my_project/my_app/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("this is my django project practices demo")
+
+def form(request):
+    return HttpResponse("this is my django project practices demo")
